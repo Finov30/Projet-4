059ebfb919e6f410b07211ad2ccd382dd067d231
Finov30 <samu.abid@gmail.com>
2024-06-28 15:34:29 +0200

    Secondss

--- auth.py
+++ auth.py
@@ -1,16 +1,49 @@
+from sqlalchemy import create_engine
 from supabase import create_client, Client
+import psycopg2
 
-url: str = "your_supabase_url"
-key: str = "your_supabase_key"
-supabase: Client = create_client(url, key)
 
-# Authenticate user
-email = "user@example.com"
-password = "password123"
+database_url = "postgresql://postgres.mmsucpjcvccdxjhymgrc:Elidevia2023!@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
 
-response = supabase.auth.sign_in(email=email, password=password)
+engine = create_engine(database_url)
 
-if response["status_code"] == 200:
-    print("User authenticated successfully!")
-else:
-    print("Authentication failed.")
+try:
+    with engine.connect() as conn:
+        print("Connected to the database.")
+except Exception as e:
+    print(f"Unable to connect to the database: {e}")
+
+supabaseUrl: str = "https://mmsucpjcvccdxjhymgrc.supabase.co"
+supabaseKey: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tc3VjcGpjdmNjZHhqaHltZ3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0NzU1MTcsImV4cCI6MjAzNTA1MTUxN30.dyGLXnPKw4LJZhhU-Mxql0Fhlw-pKFgrapHQUtsZ5tg"
+
+supabase = create_client(supabaseUrl, supabaseKey)
+
+def signup(email, password):
+    """Inscription d'un nouvel utilisateur."""
+    user, error = supabase.auth.sign_up({
+        "email": email,
+        "password": password
+    })
+    if error:
+        # Gérez l'erreur ici
+        print(error)
+    else:
+        print("User signed up successfully.")
+
+def login(email, password):
+    """Connexion d'un utilisateur."""
+    user, error = supabase.auth.sign_in(email=email, password=password)
+    if error:
+        print(f"Erreur de connexion : {error.message}")
+    else:
+        print("Connexion réussie :", user)
+
+# Exemple d'utilisation
+email = "Abtest25@domaine.com"
+password = "motdepasse"
+
+# Inscription
+signup(email, password)
+
+# Connexion
+login(email, password)
